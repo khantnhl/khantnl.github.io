fb246365a05d125bdc5480d751e10ac43d3a3bd0
Hlaing, Khant Nyi <knyihlaing@go.pasadena.edu>
2024-12-05 16:10:11 -0800

    update me

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -18,8 +18,9 @@ const Card = () => {
 
             <div className="card-content">
                 <p> 
-                My name is Khant, and I a junior soon Tranfering to UC. 
-                Click on the contact tab to contact me.
+                My name is Khant, and I am a junior tranferring to one of UCs from community college, hopefully to Berkeley.
+                I have previously interned at Caltech and the BreakThroughTechAI program at UCLA where I worked with teams on machine learning and full-stack projects.
+                I haven't really get back to gaming because of the CS grind but I read Japanese manga occasionally; we can talk all day! Click on the contact tab to contact me.
                 </p>
             </div>
         </div>
